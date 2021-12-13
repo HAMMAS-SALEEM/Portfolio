242fe269cf8c9d4b26952d88822ccbc205a92457
Hammas-Saleem <hammassaleem376@gmail.com>
2021-12-14 00:18:26 +0500

    Fix eslint error for Portfolio

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const main = document.querySelector('.mobile_menu')
+const main = document.querySelector('.mobile_menu');
 
-function openMain () {
-    main.style.visibility = 'visible'
+function openMain() {
+  main.style.visibility = 'visible';
 }
 
-function closeMain () {
-    main.style.visibility = 'hidden'
+function closeMain() {
+  main.style.visibility = 'hidden';
 }
+
+openMain();
+closeMain();
