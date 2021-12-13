507a012c434f47ea59bf2659db5ee1930195dc43
Hammas-Saleem <hammassaleem376@gmail.com>
2021-12-13 22:57:50 +0500

    Update mobile menu for Portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+const menu = document.getElementById("#mobile_menu");
+function openMenu () {
+    menu.style.visibility = 'visible';
+}
+function closeMenu () {
+    menu.style.visibility = 'hidden';
+}
+openMenu();
+closeMenu();
