44a363c5fcefe5cb45184c353053a5de76467b0c
Hammas-Saleem <hammassaleem376@gmail.com>
2021-12-13 23:19:02 +0500

    Update nav menu for Portfolio

---
+++ app.js
@@ -0,0 +1,9 @@
+const main = document.querySelector('.mobile_menu')
+
+function openMain () {
+    main.style.visibility = 'visible'
+}
+
+function closeMain () {
+    main.style.visibility = 'hidden'
+}
