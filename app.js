1cfea85bb499853b2480a829a79dde406e1c0235
Hammas-Saleem <hammassaleem376@gmail.com>
2021-12-14 18:47:17 +0500

    Update modal for Portfolio

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const main = document.querySelector('.mobile_menu');
+const modal = document.querySelector('.modal');
+const body = document.body;
 
 function openMain() {
   main.style.visibility = 'visible';
@@ -6,6 +8,16 @@ function openMain() {
 
 function closeMain() {
   main.style.visibility = 'hidden';
+}
+
+function openModal () {
+  modal.style.visibility = 'visibile'
+  // body.style.overflow = 'hidden'
+}
+
+function closeModal() {
+  modal.style.visibility = 'hidden'
+  body.style.overflow = 'visible'
 }
 
 openMain();
