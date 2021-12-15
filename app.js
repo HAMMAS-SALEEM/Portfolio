f60153e6ce3fe5b3eb79373ed641cb4de4444732
Hammas-Saleem <hammassaleem376@gmail.com>
2021-12-15 05:03:12 +0500

    Update markup and add dynamic elements to it

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const main = document.querySelector('.mobile_menu');
 const modal = document.querySelector('.modal');
 const { body } = document;
-const dynamicWork = document.querySelector('.works2');
+const dynamicWork = document.querySelector('.works');
 
 function openMain() {
   main.style.visibility = 'visible';
@@ -16,7 +16,8 @@ closeMain();
 
 const array = [{
   name: 'Tonic',
-  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+  shortDescription:'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  longDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
   featured: ['CANOPY', 'Back End Dev', 2015],
   image: 'images/SP.png',
   imagedesktop: 'images/Snap01.png',
@@ -26,7 +27,8 @@ const array = [{
 },
 {
   name: 'Multi-Post Stories',
-  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+  shortDescription:'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  longDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
   featured: ['CANOPY', 'Back End Dev', 2015],
   image: 'images/mps2.png',
   imagedesktop: 'images/snap02.png',
@@ -36,7 +38,8 @@ const array = [{
 },
 {
   name: 'Tonic',
-  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+  shortDescription:'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  longDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
   featured: ['CANOPY', 'Back End Dev', 2015],
   image: 'images/mps.png',
   imagedesktop: 'images/snap03.png',
@@ -46,7 +49,8 @@ const array = [{
 },
 {
   name: 'Multi-Post Stories',
-  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+  shortDescription:'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  longDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
   featured: ['CANOPY', 'Back End Dev', 2015],
   image: 'images/mps2.png',
   imagedesktop: 'images/snap04.png',
@@ -56,9 +60,48 @@ const array = [{
 },
 ];
 
+// function tech () {
+//   for(let i=0; i<=array.length; i++){
+//     for(let j=1; j<array[i].featured.length; j++){
+
+//     }
+//   }
+// }
+
+function arrElem () {
+  for(let i=0; i<array.length; i++){
+    for(let j=1; j<=array[i].featured.length-1;j++){
+     document.write(`<li>${array[i].featured[j]}</li>`)
+    } 
+  }
+}
+
+
 
 for(let i=0; i<array.length; i++){
-document.write(
- `<div class='cr-grid'><div class='w1contain'><img class='pti' src='./images/SP.png' alt='Portfolio Title Image'><img class='snap01 img13' src='./images/Snap01.png' alt='Portfolio'><div class='for-fullscreen'><header><h2 class='works-title'>${array[i].name}</h2></header><ul class='specs w11contain'><li class='spec1'>CANOPY</li><li class='spec2'>Back End Dev</li><li class='spec2'>2015</li></ul><p class='w1par'>A daily selection of privately personalized reads; no accounts or sign-upsrequired.</p><div class='bcontain'><button type='button' class='w1btn' disabled>html</button><button type='button' class='w1btn w1b' disabled>css</button><button type='button' class='w1btn w1b' disabled>javaScript</button></div><button onclick='openModal()' type='button' class='w1btn2'>See Project</button></div></img></img></div></div>`
- )
+dynamicWork.innerHTML += `<div class="cr-grid">
+  <div class="w1contain">
+      <img class="pti" src="./images/SP.png" alt="Portfolio Title Image"/>
+      <img class="snap01 img13" src="./images/Snap01.png" alt="Portfolio"/>
+      <div class="for-fullscreen">
+          <header>
+              <h2 class="works-title">${array[i].name}</h2>
+          </header>
+          <ul class="specs w11contain">
+              <li class="spec1">${array[i].featured[0]}</li>
+              <li class="spec2">${array[i].featured[i]}</li>
+          </ul>
+          <p class="w1par">${array[i].shortDescription}
+          </p>
+          <div class="bcontain">
+              <button type="button" class="w1btn" disabled>html</button>
+              <button type="button" class="w1btn" disabled>css</button>
+              <button type="button" class="w1btn" disabled>javaScript</button>
+          </div>
+          <button onclick="openModal()" type="button" class="w1btn2">See Project</button>
+      </div>
+  </div>
+  </div> 
+  `
+
 }
